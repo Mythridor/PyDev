39bf5c306b5e4f0b10286934da33be7951a52b4f
Mythridor <enoubia@gmail.com>
2017-11-15 12:15:49 +0100

    Adding subskill to worksheet

--- xlsDev/output.py
+++ xlsDev/output.py
@@ -11,7 +11,8 @@ def output():
     wb2 = xlwt.Workbook()
     Sheet1 = wb2.add_sheet('Evaluation')
     Sheet1.write(0, 0, 'Compétence')
-    col = 1
+    Sheet1.write(0, 1, 'Sous-compétence')
+    col = 2
     for mtr in range(1, 5):
         Sheet1.write(0, col, mtr)
         col += 1
@@ -21,11 +22,13 @@ def output():
     subcp = 1
     for ctr in range(1, 250):
         cp = str((s.cell(ctr, 2).value).encode('utf-8').decode('utf-8'))
-        print()
-        print(s.cell(ctr, 5).value == 1.0)
         if s.cell(ctr, 5).value == 1.0:
             Sheet1.write(row, 0, cp)
             row += 1
+
+            if s.cell(ctr, 3).value != "":
+                Sheet1.write(row, subcp, s.cell(ctr, 3).value)
+
         wb2.save('test.xls')
 
 
