a063ef46ee01a8622e97565dae4882219b8e9666
klomlk <sylvain.ressier@gmail.com>
2017-11-23 15:40:36 +0100

    This version enables editing fields: writing and changing the background colour.

--- xlsDev/test/newOutput.py
+++ xlsDev/test/newOutput.py
@@ -2,15 +2,15 @@ from xlwt import easyxf
 from xlutils.copy import copy
 from xlrd import open_workbook
 def output():
-    source = open_workbook("ASIP_source.xlsx", formatting_info=True)
-    s = source.sheet_by_index(0)
+    source = open_workbook("ASIP_source.xls", formatting_info=True)
+    wb = copy(source)
+    ws = wb.get_sheet(0)
 
     #sheet_test1 = book.add_sheet("test1")
     #sheet_test1.write(0,0,"Première colonne")
     #sheet_test1.write(0,1,"Deuxième colonne")
-
-
-    Nom = s.write(5,1,"Test")
-    source.save("mystesto.xlsx")
-
+    plain = easyxf('font: name Calibri, height 220; alignment: horizontal center, vertical center; pattern: pattern solid,fore_color yellow')
+    ws.write(2,3,"Test",plain)
+    wb.save("mystesto.xls")
+    #print(xls_write)
 output()
