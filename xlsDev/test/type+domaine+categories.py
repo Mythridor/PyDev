5e33056a59c2530b65f2b9b426029291617746df
klomlk <sylvain.ressier@gmail.com>
2017-11-24 16:43:36 +0100

    Création de liste pour chaque ligne

--- xlsDev/test/type+domaine+categories.py
+++ xlsDev/test/type+domaine+categories.py
@@ -4,24 +4,13 @@ from xlrd import open_workbook
 
 def final():
     matrix = open_workbook("matrix.xls", formatting_info=True).sheet_by_index(1)
-    liste=[]
-    dico ={}
-
+    liste=list()
+    #dico ={}
+    #print(matrix.cell(32,0).value)
     for row_x in range(3,matrix.nrows):
-        for col_x in range(0,3):
-            #cell = matrix.cell(row_x,col_x)
-            #try:
-            if matrix.cell(row_x,0).value == '':
-                pass
-            elif matrix.cell(row_x,col_x).value !='':
-                dico[matrix.cell(row_x,0).value]=list()
-                col_x+=1
-                dico[matrix.cell(row_x, 0).value].append(matrix.cell(row_x, col_x).value)
-            #else:
-             #       if matrix.cell(row_x,col_x).value == '':
-              #          dico[matrix.cell(row_x, 0).value].append(' ')
-                #    dico[matrix.cell(row_x,0).value].append(matrix.cell(row_x,col_x))
-            #except KeyError:
-            #    continue
-        print(dico)
+        intermediaire = list()
+        for col_x in range(0,4):
+            intermediaire.append(matrix.cell(row_x, col_x).value)
+        liste.append(intermediaire)
+    print(liste)
 final()
