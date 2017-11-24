ab64f08b8a2b4ad9dda2ef1a38a3c3a9389ab1e7
klomlk <sylvain.ressier@gmail.com>
2017-11-24 14:42:30 +0100

    test de "merge"

--- xlsDev/test/type_competence.py
+++ xlsDev/test/type_competence.py
@@ -4,14 +4,15 @@ from xlrd import open_workbook
 
 def type_competence():
     matrix = open_workbook("matrix.xls",formatting_info=True).sheet_by_index(1)
-    liste = set()
+    liste = []
     for row_x in range(0,matrix.nrows):
         cell = matrix.cell(row_x,0)
         if cell.value == "":
             continue
         else:
-            liste.add(cell.value)
-    for i in liste:
-        print(i)
-    return liste
+            liste.append(cell.value)
+    liste.pop(0)
+    setto=set(liste)
+    print(setto)
+    return setto
 type_competence()
