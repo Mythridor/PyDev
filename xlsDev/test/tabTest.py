77760f4af006ebc87f34dcb7d98588c92b04935d
Mythridor <enoubia@gmail.com>
2017-11-24 16:52:16 +0100

    Fixing faulty test merging function

--- xlsDev/test/tabTest.py
+++ xlsDev/test/tabTest.py
@@ -6,21 +6,23 @@ from xlrd import open_workbook
 wb2 = xlwt.Workbook()
 s = wb2.add_sheet('Evaluation', cell_overwrite_ok=True)
 
-wb = open_workbook("test.xls")
-s2 = wb.sheet_by_index(0)
 listeComp = [
-    [1, 2, 3],
-    [1, 4, 5],
-    [1, 6, 7]
+    ["test", "a", "e"],
+    ["test", "b", "f"],
+    ["test", "c", "g"]
 ]
 
-range = range(0, 3)
-for y in range:
-    for x in range:
-        condition = s2.cell(y - 1, x).value == s2.cell(y, x).value and y - 1 >= 0
-        s.write(y, x, listeComp[y][x])
-        if condition:
-            print(str(condition) + ":" + str(y - 1) + "/" + str(y) + "/" + str(x))
+for row in range(0, 3):
+    for col in range(0, 3):
+        s.write(row, col, listeComp[row][col])
+
+wb2.save('test.xls')
+
+wb = open_workbook("test.xls")
+s2 = wb.sheet_by_index(0)
+for y in range(0, 3):
+    for x in range(0, 3):
+        if y - 1 >= 0 and s2.cell(y - 1, x).value == s2.cell(y, x).value:
             s.write_merge(y - 1, y + 1, x, x, listeComp[y][x])
 
 wb2.save('test.xls')
