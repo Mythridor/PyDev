f8af4dc5301423da5472a3f8ae5a1af1fa6baf30
klomlk <sylvain.ressier@gmail.com>
2017-11-29 18:52:55 +0100

    Tiro finale: - Réalise une liste de compétences correspondant à la colonne de la fonction sélectionnée - Parcour la liste par item et par élément - Mise en place d'un compteur puis d'un offset de 20 lignes.

--- xlsDev/test/Tiro_finale.py
+++ xlsDev/test/Tiro_finale.py
@@ -4,50 +4,58 @@
 # Description: script permettant de générer une fiche d'évaluation xls
 
 ####### Importation des scripts essentiels #######
-import selection_fonctions
+from xlrd import open_workbook
 import type_domaine_categories
+from xlutils.copy import copy
+import selection_fonctions
+from xlwt import easyxf
 import get_colon
+#import tabTest2
 import get_ones
 import re
 #######
 
-confirmation='' # Critère d'arrêt de boucle while
-liste_fonctions = selection_fonctions.select_job() # Liste de fonctions
+confirmation = ''  # Critère d'arrêt de boucle while
+liste_fonctions = selection_fonctions.select_job()  # Liste de fonctions
 liste_competences = type_domaine_categories.get_skills()  # Liste des compétences
 
-while True :
-    if confirmation =="O" or confirmation =="Oui" or confirmation =='OUI' \
-            or confirmation =='yes' or confirmation =='Y':
-        break # condition d'arrêt de boucle
+while True:
+    if confirmation == "O" or confirmation == "Oui" or confirmation == 'OUI' \
+            or confirmation == 'yes' or confirmation == 'Y':
+        break  # condition d'arrêt de boucle
     if confirmation == "q" or confirmation == "exit" or confirmation == "quit":
         break
-    sujet_fonction = str.capitalize(input("Quel est la fonction du sujet?\n"))
+    sujet_fonction = str.capitalize(input("Quel est la fonction du sujet? (q pour quitter)\n"))
     pattern = re.compile(sujet_fonction)
 
-    match = list(filter(pattern.match,liste_fonctions)) # noms de fonctions correspondant à la recherche
+    match = list(filter(pattern.match, liste_fonctions))  # noms de fonctions correspondant à la recherche
 
     if len(match) > 1:
         print("Plusieurs intitulés de fonctions correspondent à votre recherche. Voici la liste:")
         for i in match:
             print(i)
     else:
-        print('Vous avez choisi la fonction suivante:',match[0])
+        print('Vous avez choisi la fonction suivante:', match[0])
         confirmation = str.capitalize(input("Confirmez-vous la sélection? [oui/non]\n"))
-        if confirmation == "Oui" or confirmation =="O" or confirmation =='OUI' or confirmation =='yes' or confirmation =='Y':
+        if confirmation == "Oui" or confirmation == "O" or confirmation == 'OUI' or confirmation == 'YES' \
+                or confirmation == 'Y':
             col_x = get_colon.select_colon(match[0])
             ones = get_ones.values(col_x[0])
             x = 0
             Skills4Job = list()
-            for x in range(0,len(ones)-1):
-                #print(ones[x], type(ones[x]))
-                #print(len(liste_competences))
-                #print(liste_competences[x])
+            for x in range(0, len(ones)-1):
                 if ones[x][0] == '':
                     continue
-                #elif ones[x] == "1.0":
-                 #   print(liste_competences[x])
+                # elif ones[x] == "1.0":
+                #   print(liste_competences[x])
                 elif ones[x][0] == 1.0:
                     Skills4Job.append(liste_competences[x])
-            for i in Skills4Job:
-                print(i)
-
+            longueur = len(Skills4Job)
+            buffer = 20
+            for count, data in enumerate(Skills4Job):
+                # count est le compteur de lignes (c'est un chiffre integer)
+                # data correspond à l'item numéro "count" de Skill4Job (data = Skill4Job[count])
+                for element in range(0, 4):
+                    # element est l'index entre 0 et 4 pour chaque élément de la liste data
+                    offset = count+buffer
+                    print(data[element], offset) # chaque count est décalée de 20 lignes.
