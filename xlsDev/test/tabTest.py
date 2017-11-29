7312cac8a4fa43caf402585f06e5830d29093a30
Mythridor <enoubia@gmail.com>
2017-11-29 12:24:28 +0100

    Commenting merging function to show array interpretation function

--- xlsDev/test/tabTest.py
+++ xlsDev/test/tabTest.py
@@ -22,17 +22,17 @@ for row in range_y:
 
 wb2.save('test.xls')
 
-wb = open_workbook("test.xls")
-s2 = wb.sheet_by_index(0)
-n = 0
-for row in range_y:
-    for col in range_x:
-        print(row, " ", row - n, " ", n, col == (len(range_x) - 1))
-        # print((s2.cell(row - 1, col).value == s2.cell(row, col).value), ":", s2.cell(row - 1, col).value, "[", row - 1,"] / ", s2.cell(row, col).value, "[", row, "] ")
-        if (row - 1 >= 0) and (s2.cell(row - 1, col).value == s2.cell(row, col).value):
-            s.write_merge(row - n, row, col, col, listeComp[row][col])
-
-        if col == (len(range_x) - 1):
-            n += 1
-
-wb2.save('test.xls')
+# wb = open_workbook("test.xls")
+# s2 = wb.sheet_by_index(0)
+# n = 0
+# for row in range_y:
+#    for col in range_x:
+#        print(row, " ", row - n, " ", n, col == (len(range_x) - 1))
+#        # print((s2.cell(row - 1, col).value == s2.cell(row, col).value), ":", s2.cell(row - 1, col).value, "[", row - 1,"] / ", s2.cell(row, col).value, "[", row, "] ")
+#        if (row - 1 >= 0) and (s2.cell(row - 1, col).value == s2.cell(row, col).value):
+#            s.write_merge(row - n, row, col, col, listeComp[row][col])
+#
+#        if col == (len(range_x) - 1):
+#            n += 1
+#
+# wb2.save('test.xls')
