c5350da94d57be52b8bbf6d35ab5ca799dca1c72
klomlk <sylvain.ressier@gmail.com>
2017-11-30 10:38:05 +0100

    Refactorisation du code

--- xlsDev/test/Tiro_finale.py
+++ xlsDev/test/Tiro_finale.py
@@ -5,19 +5,76 @@
 
 ####### Importation des scripts essentiels #######
 from xlrd import open_workbook
-import type_domaine_categories
 from xlutils.copy import copy
-import selection_fonctions
 from xlwt import easyxf
-import get_colon
-import tabTest2
-import get_ones
 import re
+
+def get_skills():
+    matrix = open_workbook("matrix.xls", formatting_info=True).sheet_by_index(1)
+    liste=list()
+    for row_x in range(3,matrix.nrows):
+        intermediaire = list()
+        for col_x in range(0,4):
+            intermediaire.append(matrix.cell(row_x, col_x).value)
+        liste.append(intermediaire)
+    liste.pop(-1)  # suppression d'une liste vide
+    return liste
+
+def select_job():
+    matrice = open_workbook("matrix.xls", formatting_info=True)
+    jobs = matrice.sheet_by_index(1)
+    liste=[]
+    for col_x in range(0,jobs.ncols):
+        cell = jobs.cell(0,col_x)
+        if cell.value == '':
+            continue
+        else:
+            liste.append(cell.value)
+    liste.pop(0) # Suppression de "Fonction"
+    liste.pop(-1) # Suppression du dernier élément
+    return liste
+
+def values(col_x):
+    matrix = open_workbook("matrix.xls", formatting_info=True).sheet_by_index(1)
+    liste = list()
+    for row_x in range(3, matrix.nrows):
+        intermediaire = list()
+        intermediaire.append(matrix.cell(row_x, col_x).value)
+        liste.append(intermediaire)
+    liste.pop(-1)  # suppression d'un élément vide
+    return liste
+
+def select_colon(fonction):
+    matrice = open_workbook("matrix.xls", formatting_info=True)
+    jobs = matrice.sheet_by_index(1)
+    colonne =list()
+    for col_x in range(0,jobs.ncols):
+        cell = jobs.cell(0,col_x)
+        if cell.value == fonction:
+            colonne.append(col_x)
+    return colonne
+
+def writeout(lista):
+    source = open_workbook("test.xls", formatting_info=True, )  # Ouverture du fichier test.xls
+    wb = copy(source)  # Passage de format xlrd à xlwt
+    #ns = wb.add_sheet("Test-ecriture", cell_overwrite_ok=True)  # Création d'une feuille vierge pour tester
+    ws = wb.get_sheet(0)  # Sélection de la page "Test-ecriture" dans fichier source
+    buffer = 19
+    plain = easyxf('font: name Calibri, height 220')
+    for count, data in enumerate(lista):
+        # count est le compteur de lignes (c'est un chiffre entier)
+        # data (qui est une liste) correspond à l'item numéro "count" de Skill4Job (data = Skill4Job[count])
+        for element in range(0, 4):
+            # element est l'index entre 0 et 4 pour chaque élément de la liste data
+            offset = count + buffer # chaque count est décalée de 20 lignes.
+            ws.write(offset,element,data[element],plain) # ici element correspond également à la colonne de chaque liste
+            print(data[element], offset)
+    wb.save('output_write.xls')
 #######
 
 confirmation = ''  # Critère d'arrêt de boucle while
-liste_fonctions = selection_fonctions.select_job()  # Liste de fonctions
-liste_competences = type_domaine_categories.get_skills()  # Liste des compétences
+liste_fonctions = select_job()  # Liste de fonctions
+liste_competences = get_skills()  # Liste des compétences
 
 while True:
     if confirmation == "O" or confirmation == "Oui" or confirmation == 'OUI' \
@@ -39,18 +96,16 @@ while True:
         confirmation = str.capitalize(input("Confirmez-vous la sélection? [oui/non]\n"))
         if confirmation == "Oui" or confirmation == "O" or confirmation == 'OUI' or confirmation == 'YES' \
                 or confirmation == 'Y':
-            col_x = get_colon.select_colon(match[0])
-            ones = get_ones.values(col_x[0])
+            col_x = select_colon(match[0])
+            ones = values(col_x[0])
             x = 0
             Skills4Job = list()
             for x in range(0, len(ones)-1):
                 if ones[x][0] == '':
                     continue
-                # elif ones[x] == "1.0":
-                #   print(liste_competences[x])
                 elif ones[x][0] == 1.0:
                     Skills4Job.append(liste_competences[x])
             longueur = len(Skills4Job)
             # Passage à l'écriture
-            tabTest2.writeout(Skills4Job)
+            writeout(Skills4Job)
 
